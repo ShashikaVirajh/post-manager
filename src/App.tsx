8b93577e9af73c57b8f8a2d2f0a8dc51ded2c40f
Shashika Virajh <shashikasvka@gmail.com>
2022-09-11 11:55:56 +0530

    Setup Initial Structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Test } from 'components/test.component';
+
+const App = () => {
+  return (
+    <>
+      <div>Hi Im the app</div>
+      <Test />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/test.component.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Test = () => {
+  return <div>Hi Im a component</div>;
+};
