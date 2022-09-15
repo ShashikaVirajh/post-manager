3db52ea264b555afa2521164d831f74418feb624
nimeshikagit <nimemobile@gmail.com>
2022-09-15 23:47:49 +0530

    header nav bar

--- src/components/header/header-logged-in.tsx
+++ src/components/header/header-logged-in.tsx
@@ -1,3 +1,43 @@
-const HeaderLoggedIn = (): JSX.Element => <div>HeaderLoggedIn</div>;
+import { Button } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
 
-export default HeaderLoggedIn;
+export const HeaderLoggedIn = (): JSX.Element => {
+  const navigate = useNavigate();
+
+  const handleLogout = (): void => {
+    navigate('/');
+  };
+
+  return (
+    <>
+      <Link style={styles.linkContainer} to={'/create-post'}>
+        Create Post
+      </Link>
+
+      <Button onClick={handleLogout} sx={styles.btn}>
+        Sign Out
+      </Button>
+    </>
+  );
+};
+
+const styles = {
+  linkContainer: {
+    textDecoration: 'none',
+    color: '#fff',
+    fontSize: '0.875rem',
+    lineHeight: 1.75,
+    letterSpacing: ' 0.02857em',
+    fontWeight: '500',
+    marginRight: '2rem',
+    background: '#28a745',
+    padding: '0.5rem',
+    borderRadius: '0.25rem',
+  },
+
+  btn: {
+    background: '#6c757d',
+    color: 'white',
+    padding: 1,
+  },
+};
